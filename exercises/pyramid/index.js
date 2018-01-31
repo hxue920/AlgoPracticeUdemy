273df6cc7ba404f3d4970f20c6f052108f3cc68f
hxue920 <devilsing@gmail.com>
2018-01-30 23:16:13 -0800

    add solution to pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let colCount = Math.floor((2*n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let stair = '#';
+    let count = colCount;
+    let temp = 0;
+    while (count < 2*n - 1 - 1) {
+      if (temp <= row - 1) {
+        stair = '#' + stair + '#';
+        temp++;
+      } else {
+        stair = ' ' + stair + ' ';
+      }
+      count++;
+    }
+    console.log(stair);
+  }
+}
 
 module.exports = pyramid;
