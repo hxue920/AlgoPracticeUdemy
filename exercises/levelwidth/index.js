f81db3f6044cdf3b2f96d25d8ee55250c424c491
hxue920 <devilsing@gmail.com>
2018-02-04 16:17:52 -0800

    add solution for levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const queue = [[root, 0]];
+  const counter = [];
+  while (queue.length) {
+    let node = queue.shift();
+    if (!counter[node[1]]) {
+      counter[node[1]] = 1;
+    } else {
+      counter[node[1]] += 1;
+    }
+    for (let child of node[0].children) {
+      queue.push([child, node[1]+1]);
+    }
+  }
+  return counter;
+}
 
 module.exports = levelWidth;
