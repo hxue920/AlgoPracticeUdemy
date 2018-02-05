37c4d7ea942015a9cf5fae639afb6a48701a4e05
hxue920 <devilsing@gmail.com>
2018-02-04 23:52:15 -0800

    add solution to events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,33 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.storage = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
-
+  on(eventName, callback) {
+    if (!this.storage[eventName]) {
+      this.storage[eventName] = [callback];
+    } else {
+      this.storage[eventName].push(callback);
+    }
+  }
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (!this.storage[eventName]) {
+      return;
+    }
+    for (let event of this.storage[eventName]) {
+      event();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.storage[eventName];
+  }
 }
 
 module.exports = Events;
